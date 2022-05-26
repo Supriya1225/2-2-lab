4824dba2f7b15ff26b58826f3e7d67f243e1b772
Supriya1225 <96834714+Supriya1225@users.noreply.github.com>
2022-05-26 18:10:04 +0530

    Update pipe1.c

--- IPC/pipe1.c
+++ IPC/pipe1.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+int main() {
+int pipefds[2];
+int returnvalue;
+char writemsgs[2][20]={"Hi","Hello"};
+char readmsg[20];
+returnvalue = pipe(pipefds);
+if (returnvalue == -1) {
+printf("Unable to create pipe\n");
+return 1;
+}
+printf("Writing-Msg1 is %s\n", writemsgs[0]);
+write(pipefds[1], writemsgs[0], sizeof(writemsgs[0]));
 
+read(pipefds[0], readmsg, sizeof(readmsg));
+printf("Reading-Msg1 is %s\n", readmsg);
+printf("Writing-Msg2 is %s\n", writemsgs[0]);
+write(pipefds[1], writemsgs[1], sizeof(writemsgs[0]));
+read(pipefds[0], readmsg, sizeof(readmsg));
+printf("Reading-Msg2 is %s\n", readmsg);
+return 0;
+}
