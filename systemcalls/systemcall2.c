10ed59880cd975a7bd287a703f65f394468c6e05
Supriya1225 <96834714+Supriya1225@users.noreply.github.com>
2022-05-27 18:50:25 +0530

    Update systemcall2.c

--- systemcalls/systemcall2.c
+++ systemcalls/systemcall2.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<fcntl.h>
 
+int main(){
+	int fd[2];
+	char buf1[12] = "Hello World\n";
+	char buf2[12];
+	
+	creat("text1.txt",777);
+	
+	fd[0] = open("text1.txt",O_RDWR);
+	fd[1] = open("text1.txt",O_RDWR);
+	
+	write(fd[0],buf1,strlen(buf1));
+	write(fd[1],buf2,read(fd[1],buf2,12));
+	
+	close(fd[0]);
+	close(fd[1]);
+	
+	return 0;
+}
